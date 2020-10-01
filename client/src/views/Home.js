85f32621e85b071b48ec0c921f4941e05b311ed3
Dylan Curran <info@dylancurran.net>
2020-10-01 01:28:51 -0400

    conditional home page

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import LoadingScreen from '../components/LoadingScreen';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 const api = process.env.REACT_APP_API_URL;
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 const fetchHomepage = async () => {
   const res = await axios.get(`${api}/home/hot`, { withCredentials: true });
@@ -11,6 +21,7 @@ const fetchHomepage = async () => {
 };
 
 const Home = () => {
+  const classes = useStyles();
   const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
 
@@ -22,7 +33,21 @@ const Home = () => {
     });
   }, []);
 
-  return <div>{loading ? <LoadingScreen /> : <p>{posts[0].title}</p>}</div>;
+  return (
+    <div className={classes.root}>
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <Container maxWidth="md">
+          {posts.map((post) => (
+            <Typography variant="body1" align="center" key={post.id} paragraph>
+              {post.title}
+            </Typography>
+          ))}
+        </Container>
+      )}
+    </div>
+  );
 };
 
 export default Home;
--- server/routes/home.js
+++ server/routes/home.js
@@ -2,12 +2,35 @@ const router = require('express').Router();
 const Reddit = require('../config/snoowrap');
 
 // Get user's home page
-router.get('/hot', async (req, res, next) => {
+router.get('/:category', async (req, res, next) => {
   try {
     const r = Reddit(req.get('User-Agent'), req.session.refreshToken);
-
-    const items = await r.getHot('webdev', { limit: 10 });
-
+    const { after, before } = req.query || '';
+    let items;
+    const config = { limit: 10, after, before };
+    switch (req.params.category) {
+      case 'hot':
+        items = await r.getHot(null, config);
+        break;
+      case 'best':
+        items = await r.getBest(config);
+        break;
+      case 'top':
+        items = await r.getTop(null, config);
+        break;
+      case 'new':
+        items = await r.getNew(null, config);
+        break;
+      case 'rising':
+        items = await r.getRising(null, config);
+        break;
+      case 'controversial':
+        items = await r.getControversial(null, config);
+        break;
+      default:
+        items = await r.getHot(null, config);
+        break;
+    }
     return res.status(200).json(items);
   } catch (error) {
     next(error);
